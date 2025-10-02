72a1866cf8389701fd0d41e3b85e8bc927ef0610
valdes642 <vhvergarav@gmail.com>
2025-10-02 16:12:45 -0300

    prueba y creando las clases

---
+++ src/main/java/com/mycompany/pais/Pais.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pais;
+
+/**
+ *
+ * @author Victor Vergara
+ */
+public class Pais {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
