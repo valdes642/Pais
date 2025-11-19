9bc29be9db8e3c5066b532893239750c74072ac9
valdes642 <vhvergarav@gmail.com>
2025-11-19 19:37:32 -0300

    vamos que podemos

---
+++ src/main/java/Modelo/Conexion/ConexionDB.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo.Conexion;
+
+/**
+ *
+ * @author Victor Vergara
+ */
+public class ConexionDB {
+    
+}
